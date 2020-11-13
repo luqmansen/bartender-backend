4641315de601fd8b2370f8f513ee3b7188d2cb9c
luqmansen <luqmansen@gmail.com>
2020-11-13 14:16:23 +0700

    add image preview on admin

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from djangoql.admin import DjangoQLSearchMixin
 from django_summernote.admin import SummernoteModelAdmin
 from api.models import Gallery, Article, TourismPacket
 
 TITLE = "Website Eduwisata"
+SKIP_DISPLAY = ['created_at']
 
 admin.site.site_header = TITLE
 admin.site.site_title = TITLE
@@ -19,8 +21,14 @@ class BaseAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
 
 @admin.register(Gallery)
 class GalleryAdmin(BaseAdmin):
-    list_display = [f.name for f in Gallery._meta.fields]
+    list_display = [f.name for f in Gallery._meta.fields if f.name not in SKIP_DISPLAY]
+    list_display += ['preview_image']
     readonly_fields = []
+
+    def preview_image(self, obj):
+        return format_html(f'<img src="{obj.image.url}" width="300"/>')
+
+    preview_image.short_description = "Image"
 
 
 @admin.register(Article)
