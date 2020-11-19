64237ca6ecb892d88b8014f4842a6b48c6369d4e
luqmansen <luqmansen@gmail.com>
2020-11-19 11:47:00 +0700

    cache every endpoints for 60 second

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from api.views import GalleryViewSet, ArticleViewSet, TourismPackageViewSet
 
@@ -8,8 +9,8 @@ article_detail = ArticleViewSet.as_view({'get': 'retrieve'})
 tourism_package_list = TourismPackageViewSet.as_view({'get': 'list'})
 
 urlpatterns = [
-    path('gallery/', gallery_list),
-    path('article/', article_list, name='article-list'),
-    path('article/<str:slug>/', article_detail, name='article-detail'),
-    path('tourpackage/', tourism_package_list)
+    path('gallery/', cache_page(60)(gallery_list)),
+    path('article/', cache_page(60)(article_list), name='article-list'),
+    path('article/<str:slug>/', cache_page(60)(article_detail), name='article-detail'),
+    path('tourpackage/', cache_page(60)(tourism_package_list))
 ]
