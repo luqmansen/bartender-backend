623b530a0a91663ef37c404739f0dfd61696d926
luqmansen <luqmansen@gmail.com>
2021-01-23 06:05:28 +0700

    update checkbox template

--- egg_report/admin.py
+++ egg_report/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+from pprint import pprint
 
-# Register your models here.
+from django.contrib import admin
+from djangoql.admin import DjangoQLSearchMixin
+
+from egg_report.models import CageNum, Report
+
+SKIP_DISPLAY = ['created_at']
+
+
+class BaseAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    ordering = ('id',)
+    list_per_page = 10
+    list_display = []
+    readonly_fields = ['created_at']
+
+
+@admin.register(CageNum)
+class CageAdmin(BaseAdmin):
+    list_display = [f.name for f in CageNum._meta.fields if f.name not in SKIP_DISPLAY]
+    list_editable = [f for f in list_display if f is not 'id']
+    readonly_fields = []
+
+
+@admin.register(Report)
+class ReportAdmin(BaseAdmin):
+    list_display = [f.name for f in Report._meta.fields]
+
--- egg_report/models.py
+++ egg_report/models.py
@@ -5,6 +5,9 @@ class CageNum(models.Model):
     number = models.IntegerField(unique=True)
     is_empty = models.BooleanField(default=False)
 
+    def __str__(self):
+        return str(self.number)
+
 
 class Report(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
